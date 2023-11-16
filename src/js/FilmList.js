d8f084b2eeb2a9611a67e76160e50215ff3ea011
vadim010975 <vadim010975@mail.ru>
2023-11-16 19:20:45 +0300

    FilmList fixed

--- src/js/FilmList.js
+++ src/js/FilmList.js
@@ -6,8 +6,8 @@ export default class FilmList {
       const element = document.createElement("tr");
       element.dataset.id = el.id;
       element.dataset.title = el.title;
-      element.dataset.year = el.year;
       element.dataset.imdb = el.imdb;
+      element.dataset.year = el.year;
       element.classList.add("row");
       element.insertAdjacentHTML("beforeend", `<td class='cell'>${el.id}</td>`);
       element.insertAdjacentHTML(
@@ -16,11 +16,11 @@ export default class FilmList {
       );
       element.insertAdjacentHTML(
         "beforeend",
-        `<td class='cell'>(${el.year})</td>`
+        `<td class='cell'>imdb: ${el.imdb.toFixed(2)}</td>`
       );
       element.insertAdjacentHTML(
         "beforeend",
-        `<td class='cell'>imdb: ${el.imdb.toFixed(2)}</td>`
+        `<td class='cell'>(${el.year})</td>`
       );
       this.elements.push(element);
     });
